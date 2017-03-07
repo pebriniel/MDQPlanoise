7df636d9d7a47d35cdb5ec0767a37c63d9bd95a9
pebriniel <zeldalyoko@gmail.com>
2017-03-07 15:04:39 +0100

    update activity page and plugin

--- wp-content/plugins/mdq-event/mdq-event.php
+++ wp-content/plugins/mdq-event/mdq-event.php
@@ -54,7 +54,7 @@ function mdq_assoc_init(){
 function register_plugin_style_activite() {
     wp_register_style( 'mdq-event', plugins_url( 'mdq-event/css/css_mdq-event.css' ) );
     wp_enqueue_style( 'mdq-event' );
-}
+} 
 
 
 /**
@@ -87,6 +87,7 @@ function mdq_event_savepost($post_id, $post){
 
     // On met à jour la meta !
     update_post_meta($post_id,'mdq_event_description',$_POST['event_description']);
+    update_post_meta($post_id,'mdq_event_payement',$_POST['event_payement']);
     update_post_meta($post_id,'mdq_event_date',$_POST['event_date']);
     update_post_meta($post_id,'mdq_association_id',$_POST['event_assoc_list']);
     update_post_meta($post_id,'mdq_event_adresse',$_POST['event_adresse']);
