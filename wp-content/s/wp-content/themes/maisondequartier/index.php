2ff97b4d92f76abf1c301cca64fa9c45da2dd3eb
paulinefrqt <pauline.f@codeur.online>
2017-03-08 09:16:15 +0100

    popo 09:16 content.php

---
+++ wp-content/s/wp-content/themes/maisondequartier/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The main template file
+ *
+ * @package bootstrap-basic
+ */
+
+get_header();
+
+get_sidebar('left');
+
+		get_template_part('content', get_post_format());
+
+get_footer(); ?>
